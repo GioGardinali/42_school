0840bc0f7315893a40c7459b1701a18f5273339d
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-05-29 18:50:23 -0300

    subindo exercicios do 00 ate 04

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 17:06:28 by gigardin          #+#    #+#             */
+/*   Updated: 2023/05/29 17:26:05 by gigardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		write(1, &alphabet, 1);
+		alphabet++;
+	}	
+	write(1, &"\n", 1);
+}
---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 18:09:41 by gigardin          #+#    #+#             */
+/*   Updated: 2023/05/29 18:14:07 by gigardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'z';
+	while (alphabet <= 'a')
+	{
+		write(1, &alphabet, 1);
+		alphabet--;
+	}	
+	write(1, &"\n", 1);
+}
