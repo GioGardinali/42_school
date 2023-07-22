3d05d6ad6691e0d48b091b08553481edef9ea03a
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-07-22 19:38:18 -0300

    subindo os exercícios isprint e strlen

--- libft.a/strlen.c
+++ libft.a/strlen.c
@@ -6,7 +6,20 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:02:57 by gigardin          #+#    #+#             */
-/*   Updated: 2023/07/20 20:02:59 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/07/22 19:37:22 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	strlen(const char *s)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
