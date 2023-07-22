b8afbabadfe0e6dff43d05ab585edd8864faacfc
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-07-22 15:48:03 -0300

    subindo os primeiros exercícios da libft

--- libft.a/isalpha.c
+++ libft.a/isalpha.c
@@ -6,7 +6,19 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 15:57:32 by gigardin          #+#    #+#             */
-/*   Updated: 2023/07/20 15:57:34 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/07/22 15:46:40 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
+
+/* int	main(void)
+{
+	isalpha(c);
+	isalpha(1);
+} */
