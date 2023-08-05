a4e04a9cdeb14d57cab5e858be66ef06e6b4acdf
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-05 20:58:01 -0300

    subindo anotações de códigos

--- libft/add_functions_2/ft_strmapi.c
+++ libft/add_functions_2/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:17:58 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/03 21:19:27 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/05 20:55:45 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,37 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	int		count;
-	char	*answer;
+	char	*result;
 
 	if (!s)
 		return (NULL);
-	answer = malloc(ft_strlen(s) * sizeof(char) + 1);
-	if (!answer)
+	result = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (!result)
 		return (NULL);
 	count = 0;
-	while (s[count])
+	while (s[count] != '\0')
 	{
-		answer[count] = f(count, s[count]);
+		result[count] = f(count, s[count]);
 		count++;
 	}
-	answer[count] = '\0';
-	return (answer);
+	result[count] = '\0';
+	return (result);
 }
 
+/* Esta é a declaração da função ft_strmapi. Ela recebe dois argumentos:
+
+s: Um ponteiro constante para a string na qual a função será aplicada.
+f: Um ponteiro para uma função que será aplicada a cada caractere 
+da string. Essa função deve receber dois argumentos: 
+um unsigned int representando o índice do caractere na string e 
+um char representando o caractere em si.
+
+A função ft_strmapi cria uma nova string que contém os resultados 
+da aplicação de uma função f a cada caractere da string s. 
+O índice do caractere na string s é passado como o primeiro 
+argumento para a função f, permitindo que a função f realize 
+operações personalizadas em cada caractere. Essa função é útil 
+quando você deseja transformar ou modificar cada caractere de 
+uma string de acordo com algum critério específico. Certifique-se 
+de liberar a memória alocada para a nova string após o uso para 
+evitar vazamentos de memória. */
