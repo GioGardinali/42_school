06d89026354ce05659e9d54b765e2a1c6cfd9483
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-04 22:27:32 -0300

    subindo os textos de memchr e memcmp

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:11:04 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/02 15:44:11 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/04 22:12:07 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,29 @@ void	*ft_memchr(const void *s, int c, size_t n)
 }
 
 /* #include <stdio.h>
-
+#include <string.h>
 int	main(void)
 {
 	char *teste2;
+	char *teste3;
 	char	*teste = "testando";
 	printf("\nantes da mudança: %s", teste);
-	teste2= ft_memchr(teste, 'A', 3);
+	teste2= ft_memchr(teste, 'a', 5);
 	printf("\nvamos ver a mudança: %s", teste2);
+		printf("\nantes da mudança: %s", teste);
+	teste3= ft_memchr(teste, 'a', 5);
+	printf("\nvamos ver a mudança: %s", teste3);
 } */
+
+/* A função vai pegar um array, encontrar o que colocamos dentro do int
+dentro deste array que passamos e rodar até a quantidade de vezes que 
+solicitamos. Assim quando falamos que queremos buscar o 'a', dentro de testando,
+porém só pode rodar até o index 3, ele não vai encontrar e 
+enviará um retorno de NULL.
+Mas se eu passar um size_t de 5 para ele rodar até o index 5, 
+ele vai encontrar o 'a' dentro de testando e irá me retornar 
+tudo que vem após ele + ele (ando).
+
+OBS: para igualar as transferencias de dados, tipo um int para um char,
+temos que atribuir o type casting
+*/
--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:12:06 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/02 16:07:52 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/04 22:26:37 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 }
 
 /* #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
@@ -38,9 +39,24 @@ int	main(void)
 	char	*teste2;
 	int		resultado;
 
-	teste = "testando";
-	teste2 = "testAndo";
-	resultado = ft_memcmp(teste, teste2, 4);
-	printf("saindo o resultado da diferença entre as duas strings: \n");
+	teste = "testAndo";
+	teste2 = "testando";
+	resultado = ft_memcmp(teste, teste2, 5);
+	printf("DUPLICATA - saindo o resultado da diferença entre as duas strings: \n");
+	printf("%d\n\n", resultado);
+	resultado = memcmp(teste, teste2, 5);
+	printf("ORIGINAL - saindo o resultado da diferença entre as duas strings: \n");
 	printf("%d", resultado);
 } */
+
+/* A função vai pegar dois arrays de memória e irá comparar cada index do array até a 
+quantidade de vezes que limitamos no parâmetro 'size_t n'.
+Assim, caso ambos os arrays estiverem 100% iguais, irá retornar 0, caso ao 
+contrário, irá retornar a diferença do 1º array - 2 º array.
+p_s1 e p_s2 são atribuídos aos endereços das regiões de memória 
+apontadas por s1 e s2, respectivamente. Isso é feito para que 
+possamos percorrer os bytes individuais nas regiões de memória.
+
+OBS: para igualar as transferencias de dados, tipo um int para um char,
+temos que atribuir o type casting
+*/
