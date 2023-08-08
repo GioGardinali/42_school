69ba7424364e17adfa05fd6c378ea2348b1f81f2
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-08 20:45:43 -0300

    removendo arquivos e atualizando strtrim

--- libft/add_functions_2/ft_strtrim.c
+++ libft/add_functions_2/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:16:42 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/08 20:43:21 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/08 20:45:07 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,4 @@ int	main(void)
 	printf("saída nova de teste1: \n");
 	printf("%s", ft_strtrim(teste1, teste2));
 } */
+
