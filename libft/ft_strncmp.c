0212879c761170a982075562f29d8674903ad364
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-13 15:22:17 -0300

    subindo atualização da strncmp, pois a francinette não passou

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:10:12 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/05 18:05:42 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/13 15:20:54 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (s1[i] != '\0' && i < n)
+	if (!n)
+		return (0);
+	while (*s1 && *s2 && *s1 == *s2 && --n)
 	{
-		if (!(s1[i] == s2[i]))
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+		s1++;
+		s2++;
 	}
-	if (i != n)
-	{
-		return (s1[i] - s2[i]);
-	}
-	return (0);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 
 /* #include <unistd.h>
