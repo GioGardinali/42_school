acb9d9f6e2f06ff25c80d8b98c85ac225c90c9f5
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-15 20:55:10 -0300

    atualizando a função lstadd_front

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:23:21 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/13 16:50:08 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/15 20:54:40 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
+	new->next = lst;
+	*lst = new;
 }
+
+/* lst: O endereço de um ponteiro para o primeiro link de
+uma lista.
+novo: O endereço de um ponteiro para o nó a ser
+adicionado à lista.
+
+Adiciona o nó 'novo' no início da lista
+Eu atribuo o endereço do ponteiro new para o prinmeiro link
+( o lst), e depois eu atribuo o nó new para o primeiro.
+Para isso, eu atribuo isso ao lst que é o primeiro da lista. */
