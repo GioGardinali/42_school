29314b882f17acaeee69e8a872203f21c0078977
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-15 17:51:13 -0300

    subindo a função bonus lstnew

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:22:47 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/13 17:06:17 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/15 17:50:33 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,32 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	unsigned char	*new_node;
-	size_t			len_content;
-	size_t			index;
+	t_list	*new_node;
 
-	index = 0;
-	while ( *content)
-		index++;
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
+
+/* Lista deve consistir em 3 nós (node)
+Tem que ter um ponteiro que aponta para o 1º nó da lista
+
+Um nó tem dados e link, como exemplo:
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+Não é necessário usar type casting em nó na hora de mallocar
+Na função a gente cria um nó com o o t_list que é a saída do
+formato lista.
+assim a gente cria a variável, maloca dentro dela o 
+tipo t_list 
+w depois atribui valor no nó, no struct que a t_list
+permite, que é o content e o next, e passo o que a função 
+pediu, que o content fosse o que a função pega de parametro
+e o next, que ficasse com NULL (o next é o link do endereço) */
