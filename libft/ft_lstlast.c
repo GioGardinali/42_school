7f8e9b33222f8be1f87b3d56cfa3174ec00d9d9d
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-16 16:05:15 -0300

    alterando o makefile + o ft_lstlast

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:24:11 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/13 16:51:21 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/16 15:45:08 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-
+	t_list *last_node;
+	last_node = malloc(sizeof(t_list));
+	
+	while(lst)
+	{
+		last_node = lst->next;
+	}
+	return (last_node);
 }
+
+
+Tem que retornar o ultimo nó da lista
