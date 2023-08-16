1676ea6c6e6522b528fcc4645144c850a3829af5
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-16 15:36:27 -0300

    subindo anotações do lstsize

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:23:42 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/16 15:22:20 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/16 15:35:38 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,36 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (size);
 }
+
+/* Tá dando merda isso aqui (Arrumar) */
+/* #include	<stdio.h>
+
+int	main(void)
+{
+	t_list	*new_node;
+	t_list	*current;
+	t_list	*current2;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = 45;
+	new_node->next = NULL;
+	current = malloc(sizeof(t_list));
+	if (!current)
+		return (NULL);
+	current->content = 98;
+	current->next = NULL;
+	new_node->next = current;
+	current2 = malloc(sizeof(t_list));
+	if (!current2)
+		return (NULL);
+	current2->content = 120;
+	current2->next = NULL;
+	current->next = current2;
+
+	printf("%d \n", ft_lstsize(new_node));
+} */
 
 /* Essa função conta quantos nós temos dentro da lista encadeada
 que está na lst.
