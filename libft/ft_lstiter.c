a77998aad6135e9feaeebe7b2cd73dcee06995c0
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-16 19:49:48 -0300

    subindo a função lstiter com anotações

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:27:05 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/13 16:55:14 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/16 19:48:56 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
 }
+
+/* Temos a lista 'lst' e aplica a função
+'f' no conteúdo de cada nó.
+para isso, passamos dentro de f o content para ser modificado
+e seguido eu atribuo o novo endereço do lst para o pŕoximo nó.
+até cehgar no NULL. */
