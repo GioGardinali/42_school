143291228d0efc5ae62bef2896d903146df87a48
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-16 17:12:23 -0300

    subindo anotações do add_back

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:25:49 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/16 16:58:14 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/16 17:11:53 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		ft_lstlast(*lst)->next = new;
 }
 
-Ele quer que adicione o nó (next) do new no final
+/* Ele quer que adicione o nó (next) do new no final
 da lista.
+Eu aplico a ft_lstlast no primeiro nó lst, e mando ele passar até c
+hegar no final, ao chegar no final, eu passo o valor do nó new
+dentro do ultimo link do nó no lst. */
