eec25e55a6527a6a577d13578f70cd706697b29a
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-16 17:33:47 -0300

    subindo o lstdelone com anotações

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:26:14 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/13 16:53:54 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/16 17:30:06 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	
+	if (lst == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+	return ;
 }
+
+/* Está função vai ser responsável por realizar uma limpeza
+dentro do content do nó que vc passou.
+Assim, ele tráz a função Del e depois dá um free para
+liberar espaço na memória. */
