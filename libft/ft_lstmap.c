aa400dbb0002d2c66669f927a0820bc804e5b1db
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-16 22:57:30 -0300

    subindo atualização do Makefile

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:27:22 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/13 16:55:45 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/16 20:10:54 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new_lst;
+	t_list	*node;
+
+	if (!lst)
+		return (NULL);
+	new_lst = NULL;
+	while (lst)
+	{
+		node = ft_lstnew((*f)(lst->content));
+		if (!node)
+			ft_lstclear(&node, (*del));
+		else
+			ft_lstadd_back(&new_lst, node);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
+
+/* Itera a lista 'lst' e aplica a função'f' no conteúdo de 
+cada nó. Cria um novo lista resultante das sucessivas 
+aplicações de a função 'f'. A função 'del' é usada para
+exclua o conteúdo de um nó, se necessário. */
