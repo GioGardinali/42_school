e236bd38f3c9d2a050b141dbcdd5ac0d97c1300a
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-17 20:04:45 -0300

    subindo anotaçoes da lstmap

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:23:21 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/16 16:31:59 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/17 16:27:10 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,13 @@ Adiciona o nó 'novo' no início da lista
 Eu atribuo o endereço do ponteiro new para o prinmeiro link
 ( o lst), e depois eu atribuo o nó new para o primeiro.
 Para isso, eu atribuo isso ao lst que é o primeiro da lista. */
+
+/* new->next = *lst;: O campo next do novo elemento (new) está sendo atribuído 
+ao valor que *lst (ponteiro para o primeiro elemento da lista) está 
+apontando. Isso faz com que o novo elemento aponte para o que era o 
+primeiro elemento da lista antes de adicionar o novo elemento.
+
+*lst = new;: O ponteiro *lst, que originalmente apontava para o primeiro 
+elemento da lista, agora é atualizado para apontar para o novo elemento 
+(new). Isso faz com que o novo elemento se torne o primeiro elemento da 
+lista. */
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 20:27:22 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/16 20:10:54 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/17 20:02:12 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,37 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 cada nó. Cria um novo lista resultante das sucessivas 
 aplicações de a função 'f'. A função 'del' é usada para
 exclua o conteúdo de um nó, se necessário. */
+
+/* A função ft_lstmap é usada para criar uma nova lista encadeada aplicando 
+uma função (f) a cada elemento da lista original (lst). A função f é usada 
+para criar o conteúdo de cada novo elemento da lista resultante. Além disso, 
+a função del é usada para deletar o conteúdo de cada elemento da nova lista 
+em caso de falha na alocação de memória durante a criação dos novos elementos.
+
+ (new_lst) e um nó temporário (node) que será usado para criar novos elementos 
+ na nova lista. */
+/*  new_lst = NULL;: Inicializa o ponteiro da nova lista como nulo. Isso será 
+ usado para construir a nova lista a partir do zero.
+
+while (lst): Inicia um loop while que percorre todos os elementos da lista 
+original.
+
+node = ft_lstnew((*f)(lst->content));: Cria um novo nó (node) usando a 
+função ft_lstnew. A função f é aplicada ao conteúdo do nó atual da 
+lista original (lst->content) para criar o conteúdo do novo nó.
+
+if (!node) ft_lstclear(&node, (*del));: Verifica se a criação do novo 
+nó (node) falhou (pode ter havido um erro de alocação de memória). Se isso 
+acontecer, a função ft_lstclear é chamada para liberar a memória alocada 
+para o nó (node) e para todos os nós criados até agora na nova lista 
+(new_lst). A função del é usada para deletar o conteúdo desses nós.
+
+else ft_lstadd_back(&new_lst, node);: Caso contrário, se o novo nó foi 
+criado com sucesso, ele é adicionado ao final da nova lista usando a função 
+ft_lstadd_back.
+
+lst = lst->next;: Move o ponteiro lst para o próximo elemento da lista 
+original, avançando no loop.
+
+return (new_lst);: Retorna o ponteiro para a nova lista resultante, que 
+foi construída aplicando a função f a cada elemento da lista original. */
