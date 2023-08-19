5b93477f101a0a6c6854b0c86ec25edeb9f8e74f
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-19 17:06:39 -0300

    atualizando o header

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 16:28:39 by gigardin          #+#    #+#             */
+/*   Updated: 2023/08/19 16:50:41 by gigardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+//mandatory part 
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+//bonus
+
+#endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 16:28:47 by gigardin          #+#    #+#             */
+/*   Updated: 2023/08/19 16:51:22 by gigardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	index;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+	index = 0;
+	while (s1[index] != '\0')
+	{
+		str[index] = s1[index];
+		index++;
+	}
+	while (*s2 != '\0')
+	{
+		str[index] = *s2++;
+		index++;
+	}
+	str[index] = '\0';
+	return (str);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	index;
+
+	index = 0;
+	while (src[index] && index + 1 < size)
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	if (size > 0)
+		dst[index] = '\0';
+	index = ft_strlen(src);
+	return (index);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*copy;
+	int		index;
+	int		len;
+
+	if (*s == '\0')
+	{
+		copy = malloc (sizeof (char) * 1);
+		copy[0] = '\0';
+		return (copy);
+	}
+	len = ft_strlen (s);
+	copy = malloc (sizeof (char) * len + 1);
+	if (!copy)
+		return (NULL);
+	index = 0;
+	while (s[index])
+	{
+		copy[index] = s[index];
+		index++;
+	}
+	copy[index] = '\0';
+	return (copy);
+}
