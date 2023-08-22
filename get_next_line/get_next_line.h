676cc6c02e0d96d1ccd3cfafcf7e654e5e4de82b
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-21 21:22:06 -0300

    atualizando

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 16:28:39 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/19 16:50:41 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/21 21:14:38 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
-
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 //bonus
 
