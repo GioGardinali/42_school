adebdfbf44e32720340ab286bb9d4ef9727fdbd9
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-08-22 15:42:15 -0300

    início de anotações para o projeto gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,25 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 16:28:52 by gigardin          #+#    #+#             */
-/*   Updated: 2023/08/19 16:28:55 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/08/22 15:41:39 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+/* A função read () lê dados previamente gravados em um arquivo. 
+Se qualquer parte de um arquivo regular anterior ao fim do 
+arquivo não tiver sido gravada, read () retornará bytes com 
+valor 0. Por exemplo, lseek () permite que o deslocamento do 
+arquivo seja definido além do final dos dados existentes em 
+o arquivo. Se os dados forem gravados posteriormente neste 
+ponto, as leituras subsequentes no intervalo entre o final 
+anterior dos dados e os dados recém-gravados devem retornar 
+bytes com valor 0 até que os dados sejam gravados no 
+intervalo.
+
+Se um read () for interrompido por um sinal antes de ler 
+qualquer dado, ele deve retornar -1 com errno definido como 
+[EINTR].
+
+Se um read () for interrompido por um sinal após ter lido 
+algum dado com sucesso, ele retornará o número de bytes lidos. */
