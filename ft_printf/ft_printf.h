bd5303d0849a3ca7cc44a247d12b93430e96ad29
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-09-07 18:30:21 -0300

    subindo atualizaoes de header

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,17 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 18:14:35 by gigardin          #+#    #+#             */
-/*   Updated: 2023/09/07 18:14:36 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/09/07 18:29:36 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+//mandatory part 
+
+#endif
