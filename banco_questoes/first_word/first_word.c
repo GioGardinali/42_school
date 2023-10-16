209f0292b04bcda987e4c7abd92e8a9ce137ac90
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-10-16 20:36:26 -0300

    subindo banco de questoes + exercicio 1

---
+++ banco_questoes/first_word/first_word.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 20:34:51 by gigardin          #+#    #+#             */
+/*   Updated: 2023/10/16 20:35:54 by gigardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int i = 0;
+
+	if (argc == 2)
+	{
+		while (argv[1][i] == 32 || argv[1][i] == 9)
+			i++;
+		while ((argv[1][i] != 32 && argv[1][i] != 9) &&
+			argv[1][i])
+		{
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
+
+/* eu peço para checar se é o segundo argumento da matriz
+para iniciar a impressão, assim, eu faço outra checagem
+para imprimir sem tab (9) e sem espaço(32), quero apenas
+a string sem isso na frente
+Depois uso um segundo while para impressão 
+e por fim dou \n  */
