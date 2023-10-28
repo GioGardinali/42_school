e1104ffc8f27111d5fe3ef76163597f95fb04550
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-10-28 18:33:51 -0300

    subindo definições do ground para usar no código

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,16 +6,23 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:46:16 by gigardin          #+#    #+#             */
-/*   Updated: 2023/10/27 16:47:53 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/10/28 18:32:38 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+/*Macros*/
+# ifndef TILE_SIZE
+#  define TILE_SIZE 64
+# endif
 
-
-
+/*Sprites - brackground*/
+# define SPACE1 "./images/ground_1.png"
+# define SPACE2 "./images/ground_2.png"
+# define SPACE3 "./images/ground_3.png"
+# define SPACE4 "./images/ground_4.png"
 
 
 #endif
