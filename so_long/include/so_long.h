ce16da66cc6ff615b7052ef29b3bf2f07c51ba58
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-10-28 18:52:40 -0300

    subindo coletáveis e player, porém o player ainda não tem as imagens

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:46:16 by gigardin          #+#    #+#             */
-/*   Updated: 2023/10/28 18:32:38 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/10/28 18:49:41 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,21 @@
 # endif
 
 /*Sprites - brackground*/
-# define SPACE1 "./images/ground_1.png"
-# define SPACE2 "./images/ground_2.png"
-# define SPACE3 "./images/ground_3.png"
-# define SPACE4 "./images/ground_4.png"
+# define GROUND1 "./images/ground_1.png"
+# define GROUND2 "./images/ground_2.png"
+# define GROUND3 "./images/ground_3.png"
+# define GROUND4 "./images/ground_4.png"
 
+/*Sprites - collectible*/
+# define COLLECT1 "./images/collect_1.png"
+# define COLLECT2 "./images/collect_2.png"
+# define COLLECT3 "./images/collect_3.png"
+# define COLLECT4 "./images/collect_4.png"
+
+/*Sprites - player*/
+# define PLAYER_F "./images/player_front.png"
+# define PLAYER_R "./images/player_right.png"
+# define PLAYER_L "./images/player_left.png"
+# define CPLAYER_B "./images/player_back.png"
 
 #endif
