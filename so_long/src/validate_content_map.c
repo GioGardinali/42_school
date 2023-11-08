84322f1e04ea506efa20807f77fea81be64184ad
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-11-08 17:02:49 -0300

    check_wall foi criada, porém é uma função que preciso checar mais cenário

--- so_long/src/validate_content_map.c
+++ so_long/src/validate_content_map.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 09:41:57 by gigardin          #+#    #+#             */
-/*   Updated: 2023/11/08 15:44:59 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:59:28 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	validate_content_map( t_data *game)
 	game->map.exit_count = 0;
 	parse_map(game);
 	check_cotent_count(game);
-	//check_map_walls(game); 
+	check_wall(game);
 }
 
 static void	parse_map(t_data *game)
@@ -56,4 +56,30 @@ static void	check_count_content(t_data *game)
 		handle_error(0, "You must have at least one collectible \n", game);
 	if (game->map.exit_count != 1)
 		handle_error(0, "You must have only one exit in the Map! \n", game);
+}
+
+static void	check_wall(t_data *game)
+{
+	int	i;
+	int	columns;
+	int	rows;
+
+	i = 0;
+	columns = game->map.columns;
+	rows = game->map.rows;
+	while (i < columns)
+	{
+		if (game->map.grid_matrix[0][i] != '1' ||
+			game->map.grid_matrix[rows - 1][i] != '1')
+			handle_error(0, "The map must have surrounded by Walls \n", game);
+		i++;
+	}
+	i = 0;
+	while (i < rows)
+	{
+		if (game->map.grid_matrix[i][0] != '1' ||
+			game->map.grid_matrix[i][columns - 1] != '1')
+			handle_error(0, "The map must have surrounded by Walls \n", game);
+		i++;
+	}
 }
