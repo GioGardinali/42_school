854b8c06cb9d4db85a9b2a273545e2ffebee9483
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-11-08 13:51:31 -0300

    subindo função de end_game para pegar as funções e dar free onde é necessário

--- so_long/src/game_ending.c
+++ so_long/src/game_ending.c
@@ -6,11 +6,24 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 11:04:31 by gigardin          #+#    #+#             */
-/*   Updated: 2023/11/08 11:23:09 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/11/08 13:49:53 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+
+int	end_game(t_data *game)
+{
+	if (game->win.mlx_win)
+		close_window(game);
+	free_map(&game->map);
+	free_sprites_background(game); // criar
+	free_sprites_collectible_and_player(game); // criar
+	free_sprites_ending_and_enemy(game); // criar
+	end_mlx_connect(game); // criar
+	if (game->config_stage < 4)
+		exit (EXIT_SUCCESS);
+}
 
 void	handle_error(int error_type, char *error_msg, t_data *game)
 {
