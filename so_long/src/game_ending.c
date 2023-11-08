72d02b6595aa9967826ec8c9618efe50e526c6bb
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-11-08 11:23:44 -0300

    subindo função de erros e ajeitando ela ainda no arquivo game_ending

--- so_long/src/game_ending.c
+++ so_long/src/game_ending.c
@@ -6,14 +6,25 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 11:04:31 by gigardin          #+#    #+#             */
-/*   Updated: 2023/11/08 11:06:56 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:23:09 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void handle_error(int error_type, char *error_msg, t_data *game)
+void	handle_error(int error_type, char *error_msg, t_data *game)
 {
 	if (game->config_stage == 2)
 		free_map(&game->map);
+	if (game->config_stage == 3)
+		end_game(game);
+	if (error_type == 1)
+	{
+		ft_printf("\n%s", error_msg);
+		perror("");
+		exit (EXIT_FAILURE);
+	}
+	ft_printf ("\n%s", error_msg);
+	exit (EXIT_FAILURE); //função para sair do processo e mostrar motivo de saída,
+	//funções da stdlib.h
 }
