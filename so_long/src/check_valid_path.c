8971014582592ec9cf17986a50aad72af24b548b
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2023-11-09 18:32:31 -0300

    subindo

--- so_long/src/check_valid_path.c
+++ so_long/src/check_valid_path.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:03:27 by gigardin          #+#    #+#             */
-/*   Updated: 2023/11/09 18:24:58 by gigardin         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:32:10 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,10 +84,10 @@ static char	check_valid_tile(t_data *game, int x, int y, t_map *seen_flag)
 // Essa parte é onde inpedimos o caractere '1' de ser válido no mapa,
 //assim o boneco não segue adiante, que vai ser na outra função.
 
-void	check_neighbors(t_data *game, int coord_x, int coord_y, t_map *seen_flag)
+void	check_neighbors(t_data *game, int refe_x, int refe_y, t_map *seen_flag)
 {
-	int i;
-	int x;
-	int y;
-	
+	int	i;
+	int	x;
+	int	y;
+
 }
