dd956c2b378e5d2cb34c18b59783c88cb89e7bb9
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2024-03-13 05:19:55 -0300

    arquivo de checagem das stacks

---
+++ push_swap/src/check_max_min.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_max_min.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 15:19:30 by gigardin          #+#    #+#             */
+/*   Updated: 2024/03/13 05:19:01 by gigardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	element_int(t_element *element)
+{
+	return (*(int *) element->content);
+}
+unsigned int	ft_value_min(unsigned int a, unsigned int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+unsigned int	ft_value_max(unsigned int a, unsigned int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+char	**clear_table(char **table)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (tab[i])
+		free (tab[i++]);
+	free (tab);
+	return (NULL);
+}
