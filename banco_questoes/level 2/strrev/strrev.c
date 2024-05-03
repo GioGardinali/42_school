2d013338d2ef0b1ede1e11f4501375647307c655
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2024-05-03 08:10:41 -0300

    subindo atualizações exercícios

--- banco_questoes/level 2/strrev/strrev.c
+++ banco_questoes/level 2/strrev/strrev.c
@@ -1,21 +1,19 @@
-char    *ft_strrev(char *str)
+char *ft_strrev(char *str)
 {
-	int i = -1;
+	char temp;
 	int length = 0;
-	char temporary;
+	int i = 0;
 
+	length = 0;
+	i = 0;
 	while (str[length])
 		length++;
-	while (++i < length / 2)
-//Este loop while inicia com i incrementado em 1 
-//e continua até que i seja menor que metade 
-//do comprimento da string. Isso garante que 
-//a inversão seja feita apenas até a metade 
-//da string, pois a segunda metade já estará invertida.
+	while (i < (length - 1))
 	{
-		temporary = str[i];
-		str[i] = str[length - 1 - i];
-		str[length - 1 - i] = temporary;
-	}
+		temp = str[i];
+		str[i] = str[length - 1];
+		str[length - 1] = temp;
+		i++;
+		length--;}
 	return (str);
 }
